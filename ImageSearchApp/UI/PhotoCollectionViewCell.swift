965090c0c7f1f7353a0d4b9f736d88a335a77e58
DM <maiconshared@gmail.com>
2019-05-07 22:33:16 +0200

    Add PhotoCollectionViewCell

---
+++ ImageSearchApp/UI/PhotoCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCollectionViewCell.swift
+//  ImageSearchApp
+//
+//  Created by Diego Marcon on 07/05/19.
+//  Copyright © 2019 Diego Marcon. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+}
